638e783f6c02ea44f396cf3d1b480aac04e0349e
Aa-sheesh <aashish4912345@gmail.com>
2024-06-07 20:17:45 +0530

    function update

--- 02Basics2/05 objectsDestructureandJSON.js
+++ 02Basics2/05 objectsDestructureandJSON.js
@@ -1,14 +1,14 @@
-const course={
-    courseName:'JavaScript',
-    price:999,
-    courseInstructor:'Aashish',
-}
-const{courseInstructor}=course;//destructuring
-console.log(courseInstructor);
+// const course={
+//     courseName:'JavaScript',
+//     price:999,
+//     courseInstructor:'Aashish',
+// }
+// const{courseInstructor}=course;//destructuring
+// console.log(courseInstructor);
 
-//JSON- It looks like this:
-{
-    "courseName":"JavaScript",
-    "price":"999",
-    "courseInstructor":"Aashish"
-}
+// //JSON- It looks like this:
+// {
+//     "courseName":"JavaScript",
+//     "price":"999",
+//     "courseInstructor":"Aashish"
+// }
---
+++ 03Basics3/01 functionsAndParams.js
@@ -0,0 +1,21 @@
+// normal function
+// function sayName(){
+//     console.log("A");
+//     console.log("A");
+//     console.log("S");
+//     console.log("H");
+//     console.log("I");
+//     console.log("S");
+//     console.log("H");
+// }
+// sayName();
+
+//Function with parameters with arrow function
+// const sum=(num1,num2)=>{
+//     return num1+num2;
+// }
+// console.log(sum(5,5));
+
+function loginUserMessage(){
+    
+}
